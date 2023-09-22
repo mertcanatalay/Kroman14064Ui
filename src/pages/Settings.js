e519b05670ec8b4624f8e8764294e1de00f0c0dd
mertcanatalay <mertcanatalayy@gmail.com>
2023-09-22 08:26:45 +0300

    table added

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -4,6 +4,13 @@ import { Backdrop, Box, Button, Card, CardContent, CardMedia, CircularProgress, 
 import { useLocation, useParams, useSearchParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import Api from '../api/Api';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
 
 
 function Settings() {
@@ -14,8 +21,13 @@ function Settings() {
   const [emissionType, setEmissionType] = useState("");
   const [emissionTypeList, setEmissionTypeList] = useState([]);
 
+  const [directEmission, setDirectEmission] = useState("");
+  const [directEmissionList, setDirectEmissionList] = useState([]);
+
   const [directEmissionType, setDirectEmissionType] = useState("");
   const [directEmissionTypeList, setDirectEmissionTypeList] = useState([]);
+
+  const [d, sD] = useState(null);
 
   const loadEmissionList = async () => {
     setLoading(true);
@@ -32,10 +44,23 @@ function Settings() {
   }
 
   const loadDirectEmissionList = async () => {
-    
     setLoading(true);
     var response = await Api.post('emission/direct/list', {
+    }).then(r => r.data).catch(console.error)
+    if (response && response.success) {
+      setDirectEmissionList(response.data);
+    }
+    else (
+      console.log("başarısız")
+    )
+    setLoading(false)
+  }
+  //useEffect(() => { loadEmissionList(); }, []);
+  useEffect(() => { if (pathname == "/dogrudan") loadDirectEmissionList(); loadDirectEmissionTypeList(); }, [emissionType]);
 
+  const loadDirectEmissionTypeList = async () => {
+    setLoading(true);
+    var response = await Api.post('emission/oil/list', {
     }).then(r => r.data).catch(console.error)
     if (response && response.success) {
       setDirectEmissionTypeList(response.data);
@@ -45,9 +70,33 @@ function Settings() {
     )
     setLoading(false)
   }
-  //useEffect(() => { loadEmissionList(); }, []);
-  useEffect(() => { if (emissionType == "2854D141-57C4-470F-8A9C-DFA2F71B367B") loadDirectEmissionList(); }, [emissionType]);
 
+  const deneme = async () => {
+    directEmissionTypeList.map(item => {
+      if (directEmissionType == item.Id) {
+        return <>
+          {
+            sD(item.CalorificValue)
+          }
+        </>
+
+      }
+
+    })
+  }
+
+
+  // function createData(name, calories, fat, carbs, protein) {
+  //   return { name, calories, fat, carbs, protein };
+  // }
+
+  // const rows = [
+  //   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  //   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  //   createData('Eclair', 262, 16.0, 24, 6.0),
+  //   createData('Cupcake', 305, 3.7, 67, 4.3),
+  //   createData('Gingerbread', 356, 16.0, 49, 3.9),
+  // ];
 
   return (
     <>
@@ -65,18 +114,18 @@ function Settings() {
               <CardContent>
                 <Grid container spacing={2}>
 
-                
+
                   <Grid item sm={3} xs={12} hidden={pathname != "/dogrudan"}>
                     <FormControl fullWidth>
                       <InputLabel id="demo-simple-select-label">Doğrudan Emisyon Tipi Seçiniz</InputLabel>
                       <Select
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        value={directEmissionType}
+                        value={directEmission}
                         label="Doğrudan Emisyon Tipi Seçiniz"
-                        onChange={(e) => setDirectEmissionType(e.target.value)}
+                        onChange={(e) => setDirectEmission(e.target.value)}
                       >
-                        {Array.isArray(directEmissionTypeList) && directEmissionTypeList.map((item) => {
+                        {Array.isArray(directEmissionList) && directEmissionList.map((item) => {
                           return <MenuItem key={item.Id} value={item.Id}>{item.Name}</MenuItem>
                         })}
                       </Select>
@@ -84,17 +133,17 @@ function Settings() {
                   </Grid>
                   <Grid item sm={3} xs={12}>
                     <FormControl fullWidth>
-                      <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                      <InputLabel id="demo-simple-select-label">Tip Seçiniz</InputLabel>
                       <Select
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        //value={age}
-                        label="Age"
-                      //onChange={handleChange}
+                        value={directEmissionType}
+                        label="Tip Seçiniz"
+                        onChange={(e) => { setDirectEmissionType(e.target.value) }}
                       >
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
+                        {Array.isArray(directEmissionTypeList) && directEmissionTypeList.map((item) => {
+                          return <MenuItem key={item.Id} value={item.Id}>{item.Name}</MenuItem>
+                        })}
                       </Select>
                     </FormControl>
                   </Grid>
@@ -105,7 +154,7 @@ function Settings() {
                   </Grid>
                   <Grid item sm={1} xs={12} style={{ textAlign: 'center', marginTop: 5 }}>
                     <FormControl>
-                      <Button variant="contained">Hesapla</Button>
+                      <Button variant="contained" onClick={deneme}>Hesapla</Button>
                     </FormControl>
                   </Grid>
                 </Grid>
@@ -116,7 +165,49 @@ function Settings() {
             <Card elevation={3} sx={{ margin: 'auto', borderRadius: 3, p: 1, minHeight: 600 }} style={{ marginTop: 2, marginBottom: 20 }}>
               <CardContent>
                 <Grid container spacing={2}>
-                  asd
+
+                  <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Sağdaki değerler</TableCell>
+                          <TableCell align="right">CO2</TableCell>
+                          <TableCell align="right">Ch4</TableCell>
+                          <TableCell align="right">N20</TableCell>
+                          <TableCell align="right">Toplam tCO2e</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {/* {rows.map((row) => (
+                          <TableRow
+                            key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                            <TableCell component="th" scope="row">
+                              {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.carbs}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                          </TableRow>
+                        ))} */}
+                        <TableRow
+                          // key={row.name}
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                          <TableCell component="th" scope="row">
+                            {/* {row.name} */}
+                          </TableCell>
+                          <TableCell align="right">{d}</TableCell>
+                          <TableCell align="right">{d}</TableCell>
+                          <TableCell align="right">{d}</TableCell>
+                          <TableCell align="right">{d}</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+
                 </Grid>
               </CardContent>
             </Card>
